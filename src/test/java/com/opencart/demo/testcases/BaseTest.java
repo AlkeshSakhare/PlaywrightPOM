033c354f19bb09c9096ac8f8f3f6e1e2de1ff27e
AlkeshSakhare <akleshsakhare@yahoo.com>
2026-03-02 12:09:00 +0530

    updated run config

--- src/test/java/com/opencart/demo/testcases/BaseTest.java
+++ src/test/java/com/opencart/demo/testcases/BaseTest.java
@@ -4,29 +4,29 @@ import com.microsoft.playwright.Page;
 import com.opencart.demo.pages.HomePage;
 import com.opencart.demo.pages.LoginPage;
 import com.opencart.demo.playwrightfactory.PlaywrightFactory;
-import java.util.Properties;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.Properties;
+
 public class BaseTest {
 
-  protected HomePage homePage;
-  protected LoginPage loginPage;
-  protected Properties properties;
-  PlaywrightFactory pf;
-  Page page;
-  
-  @BeforeMethod
-  public void setUp() {
-    System.out.println("Running from BaseTest: " + this.getClass().getName());
-    pf = new PlaywrightFactory();
-    properties = pf.initProp();
-    page = pf.initPlaywright(properties);
-  }
+    protected HomePage homePage;
+    protected LoginPage loginPage;
+    protected Properties properties;
+    PlaywrightFactory pf;
+    Page page;
 
-  @AfterMethod
-  public void tearDown() {
-    page.context().close();
-    page.context().browser().close();
-  }
+    @BeforeMethod
+    public void setUp() {
+        System.out.println("Running from BaseTest: " + this.getClass().getName());
+        pf = new PlaywrightFactory();
+        properties = pf.initProp();
+        page = pf.initPlaywright(properties);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        page.context().browser().close();
+    }
 }
